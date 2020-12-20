228d4e8a8228cb1483bafbc1fbc37c91daa89081
AleksandrPriadko <68820567+AleksandrPriadko@users.noreply.github.com>
2020-12-20 13:24:24 +0200

    Update task-2.js

--- task-2.js
+++ task-2.js
@@ -1,35 +1,20 @@
 const ingredients = [
-    'Картошка',
-    'Грибы',
-    'Чеснок',
-    'Помидоры',
-    'Зелень',
-    'Приправы',
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
 ];
-console.log(ingredients);
 
-const listIngredients = document.createElement('ul');
-listIngredients.setAttribute('id', "ingredients");
-console.log(listIngredients)
+const listIngredients = document.createElement("ul");
+listIngredients.setAttribute("id", "ingredients");
+console.log(listIngredients);
 
-/* const itemsProd = ingredients.forEach(ingredient => {
-    console.log([ingredient]);
-    const listIngredient = ingredient.createElement('li');
-    listIngredient.textContent = ingredients;
-    return listIngredient;
-    //itemProd.createElement('li');
-    //console.log(createItemIngred);
-    //createItemIngred.textContent(ingredients);
-}); */
-const createItemIngred = document.createElement('li');
-//createItemIngred.textContent = [ingredients];
-ingredients.forEach(createItemIngreds => {
-        createItemIngred.textContent = createItemIngreds;
-        console.log(createItemIngred)
-    }
+const result = ingredients.forEach((createItemIngreds) => {
+  const createItemIngred = document.createElement("li");
+  createItemIngred.textContent = createItemIngreds;
+  listIngredients.appendChild(createItemIngred);
+});
 
-);
-
-
-
-//console.log(createItemIngred);
+document.body.appendChild(listIngredients);
